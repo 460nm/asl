4e3fd3eac50d3685a7771b570741f2922c4e3693
Steven Le Rouzic <steven.lerouzic@gmail.com>
2024-07-30 00:18:28 +0200

    Initial commit

---
+++ asl/base/integers.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+using int8_t  = char;
+using int16_t = short;
+using int32_t = int;
+using int64_t = long long;
+
+using uint8_t  = unsigned char;
+using uint16_t = unsigned short;
+using uint32_t = unsigned int;
+using uint64_t = unsigned long long;
+
---
+++ asl/base/integers_tests.cpp
@@ -0,0 +1,13 @@
+#include "asl/base/integers.hpp"
+
+static_assert(sizeof(int8_t)  == 1);
+static_assert(sizeof(int16_t) == 2);
+static_assert(sizeof(int32_t) == 4);
+static_assert(sizeof(int64_t) == 8);
+
+static_assert(sizeof(uint8_t)  == 1);
+static_assert(sizeof(uint16_t) == 2);
+static_assert(sizeof(uint32_t) == 4);
+static_assert(sizeof(uint64_t) == 8);
+
+int main() { return 0; }
