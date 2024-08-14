03e5d14ed191a5a600c7b9f6c33bb2b12fdd5eac
Steven Le Rouzic <steven.lerouzic@gmail.com>
2024-08-15 01:08:06 +0200

    Start work on object

--- asl/object.hpp
+++ asl/object.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
+#include "asl/ptr.hpp"
+
 namespace asl {
 
 template<typename T>
 class object final
 {
+    using wrapped = devoid_t<un_qual_t<typename ptr<T>::pointee>>;
+
+    ASL_NO_UNIQUE_ADDRESS wrapped m_value;
+    
+public:
 };
 
 } // namespace asl
--- asl/object_tests.cpp
+++ asl/object_tests.cpp
@@ -1,1 +1,14 @@
+#include "asl/object.hpp"
+
+static_assert(asl::is_object<asl::object<void>>);
+static_assert(asl::is_object<asl::object<int>>);
+static_assert(asl::is_object<asl::object<int&>>);
+static_assert(asl::is_object<asl::object<int()>>);
+
+static_assert(asl::is_empty<asl::object<void>>);
+static_assert(sizeof(asl::object<int>) == 4);
+static_assert(sizeof(asl::object<int*>) == sizeof(void*));
+static_assert(sizeof(asl::object<int&>) == sizeof(void*));
+static_assert(sizeof(asl::object<int()>) == sizeof(void*));
+
 int main() { return 0; }
