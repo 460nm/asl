696bca8521935cc1dba3ba9601187efe8bcac516
Steven Le Rouzic <steven.lerouzic@gmail.com>
2024-08-17 19:22:47 +0200

    Fix Windows build

--- asl/utility.hpp
+++ asl/utility.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
-#ifdef _MSVC_VER_
-#elif defined(__clang_version__)
-#else
+#ifndef __clang__
     #error Not a valid environment
 #endif
 
-#ifdef _MSVC_VER_
+#ifdef _MSC_VER
     #define ASL_NO_UNIQUE_ADDRESS [[msvc::no_unique_address]]
 #else
     #define ASL_NO_UNIQUE_ADDRESS [[no_unique_address]]
